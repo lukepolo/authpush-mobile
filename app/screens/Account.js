f58b0dc70c1e254ce5efaf3c8e186cea59f60b67
Luke Policinski <luke@lukepolo.com>
2018-05-08 12:37:52 -0400

    count down now works

--- app/screens/Account.js
+++ app/screens/Account.js
@@ -2,25 +2,59 @@ import { connect } from "react-redux";
 import React, { Component } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import OneTimeTokenGenerator from "./../services/OneTimeTokenGenerator";
+import TimerMixin from "react-timer-mixin";
 
-class Dashboard extends Component {
+class Account extends Component {
   constructor() {
     super();
+    this.countDown = 0;
     this.oneTimeTokenGenerator = new OneTimeTokenGenerator();
+
+    this.state = {
+      countDown: 0,
+    };
+  }
+
+  generateToken() {
+    return this.oneTimeTokenGenerator.generate(
+      this.props.navigation.getParam("account"),
+    );
+  }
+
+  updateCountDown() {
+    let epoch = Math.round(new Date().getTime() / 1000.0);
+    let countDown = 30 - epoch % 30;
+
+    if (countDown <= 0) {
+      this.generateToken();
+    }
+
+    this.setState(() => {
+      return {
+        countDown,
+      };
+    });
   }
 
   render() {
     let account = this.props.navigation.getParam("account");
+
+    this.token = this.generateToken();
+    this.timer = setInterval(this.updateCountDown.bind(this), 1000);
+
     return (
       <View>
         <Text>
           Account : {account.name} for {account.label}
         </Text>
-        <Text>
-          OTP : {this.oneTimeTokenGenerator.generate(account.credentials.key)}
-        </Text>
+        <Text>OTP : {this.token}</Text>
+        <Text>Regnerates in {this.state.countDown}</Text>
       </View>
     );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
   }
 }
 
@@ -30,4 +64,4 @@ export default connect((state) => {
   return {
     accounts: state.accountStore.accounts,
   };
-})(Dashboard);
+})(Account);
